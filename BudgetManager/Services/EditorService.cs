a9c928cbf051e82b9f33f28285330577badfd891
streerd@stud.uni-obuda.hu <streerd@stud.uni-obuda.hu>
2026-04-14 16:02:07 +0200

    Dialog Service implementálása az EditorWindow-hoz

---
+++ BudgetManager/Services/EditorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BudgetManager.Models;
+
+namespace BudgetManager.Services
+{
+    public class EditorService : IEditorService
+    {
+        public bool EditTransaction(Transaction transaction)
+        {
+            EditorWindow window = new EditorWindow(transaction);
+            return window.ShowDialog() == true;
+        }
+    }
+}
