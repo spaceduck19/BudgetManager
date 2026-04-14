b22605b69a5618421dd59348846c9527912cfc3c
streerd@stud.uni-obuda.hu <streerd@stud.uni-obuda.hu>
2026-04-14 15:40:34 +0200

    Transaction model hozzáadása, Observable property-kkel

---
+++ BudgetManager/Models/Transaction.cs
@@ -0,0 +1,43 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BudgetManager.Models
+{
+    public enum TransactionType { Income, Expense }
+
+    public partial class Transaction : ObservableObject
+    {
+        [ObservableProperty]
+        private string title = string.Empty;
+
+        [ObservableProperty]
+        private int amount;
+
+        [ObservableProperty]
+        private TransactionType type;
+
+        [ObservableProperty]
+        private string category = "General"; // például étel, utazás, fizetés
+
+        [ObservableProperty]
+        private DateTime date = DateTime.Now;
+
+        public Transaction Clone()
+        {
+            return new Transaction { Title = this.Title, Amount = this.Amount, Type = this.Type, Category = this.Category, Date = this.Date };
+        }
+
+        public void CopyFrom(Transaction other)
+        {
+            this.Title = other.Title;
+            this.Amount = other.Amount;
+            this.Type = other.Type;
+            this.Category = other.Category;
+            this.Date = other.Date;
+        }
+    }
+}
